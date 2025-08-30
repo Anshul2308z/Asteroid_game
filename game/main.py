567755e02559922cfb82bcac80456562a0ae99f4
Anshul <nemaanshul42@gmail.com>
2025-08-30 09:12:52 +0530

    before creating game loop

--- game/main.py
+++ game/main.py
@@ -1,6 +1,10 @@
+import pygame
+from constants import *
 def main():
-    print("Hello from game!")
+    pygame.init
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-
-if __name__ == "__main__":
+if __name__ == "__main__": #This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module.
     main()
